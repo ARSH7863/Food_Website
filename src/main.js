c9180112ea6539a679c4c683ab6c4a2276c7ea35
ARSH <arshs287@gmail.com>
2023-10-11 09:34:24 +0530

    Menu Section Created

--- src/main.js
+++ src/main.js
@@ -16,3 +16,42 @@ closeIcon.addEventListener('click', () => {
 hamburger.addEventListener('click', () => {
   navMenu.classList.remove('hidden');
 });
+
+const tabs = document.querySelectorAll('.tabs_wrap ul li');
+const all = document.querySelectorAll('.item_wrap');
+const foods = document.querySelectorAll('.food');
+const beverages = document.querySelectorAll('.beverage');
+const snacks = document.querySelectorAll('.snack');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    tab.classList.add('active');
+
+    const tabsVal = tab.getAttribute('data-tabs');
+    // console.log(tabsVal);
+    all.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    if (tabsVal == 'food') {
+      foods.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else if (tabsVal == 'snack') {
+      snacks.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else if (tabsVal == 'beverage') {
+      beverages.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = 'block';
+      });
+    }
+  });
+});
